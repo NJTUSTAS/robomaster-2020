61338611126c47411ae0e5508cba5a52b5dccb5b
Haowei Wen <yushijinhun@gmail.com>
2020-11-08 14:02:17 +0800

    controller: add gpio control

--- controller/index.js
+++ controller/index.js
@@ -2,6 +2,7 @@
 
 const { TagDetector } = require("./tag_detector");
 const express = require("express");
+const rpio = require("rpio");
 
 const detector = new TagDetector();
 detector.on("detect", result => {
@@ -11,6 +12,11 @@ detector.on("detect", result => {
 const app = express();
 app.post("/detector/close", (req, res) => {
     detector.close();
+    res.sendStatus(204);
+});
+app.post("/gpio/digital", (req, res) => {
+    rpio.open(req.body.pin, rpio.OUTPUT);
+    rpio.write(req.body.pin, req.body.value);
     res.sendStatus(204);
 });
 app.listen(8000);
