4c3a3ef78d8babbb36d1bac0d3c05c4ee2c18a07
raspi <raspi-njtech@yushi.moe>
2020-11-08 06:11:37 +0000

    controller: fix gpio

--- controller/index.js
+++ controller/index.js
@@ -2,6 +2,7 @@
 
 const { TagDetector } = require("./tag_detector");
 const express = require("express");
+const bodyParser = require("body-parser");
 const rpio = require("rpio");
 
 const detector = new TagDetector();
@@ -14,9 +15,12 @@ app.post("/detector/close", (req, res) => {
     detector.close();
     res.sendStatus(204);
 });
-app.post("/gpio/digital", (req, res) => {
-    rpio.open(req.body.pin, rpio.OUTPUT);
-    rpio.write(req.body.pin, req.body.value);
+app.post("/gpio/digital", bodyParser.json(), (req, res) => {
+    console.log(JSON.stringify(req.body));
+    const pin = parseInt(req.body.pin);
+    const value = parseInt(req.body.value);
+    rpio.open(pin, rpio.OUTPUT);
+    rpio.write(pin, value);
     res.sendStatus(204);
 });
 app.listen(8000);
