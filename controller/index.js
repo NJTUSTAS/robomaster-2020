5c08dc8274966d2c5e1e2f1cb8635ecb4bc5c736
Haowei Wen <yushijinhun@gmail.com>
2020-11-11 13:54:44 +0800

    controller: print tag to lcd when tag detected

--- controller/index.js
+++ controller/index.js
@@ -4,15 +4,29 @@ const { TagDetector } = require("./tag_detector");
 const express = require("express");
 const bodyParser = require("body-parser");
 const rpio = require("rpio");
+const lcd = require("./lcd");
 
 const detector = new TagDetector();
+rpio.i2cBegin();
+lcd.init();
+
 detector.on("detect", result => {
     console.log(JSON.stringify(result));
+
+    let text = "";
+    for (const det of result.detections) {
+        if (text !== "") {
+            text += " ";
+        }
+        text += det.id;
+    }
+    lcd.writeLine(text, lcd.LINE1);
 });
 
 const app = express();
 app.post("/detector/close", (req, res) => {
     detector.close();
+    rpio.i2cEnd();
     res.sendStatus(204);
 });
 app.post("/gpio/digital", bodyParser.json(), (req, res) => {
--- controller/lcd.js
+++ controller/lcd.js
@@ -5,41 +5,51 @@ const rpio = require("rpio");
  * cribbed from various python drivers.
  */
 const init = Buffer.from([0x03, 0x03, 0x03, 0x02, 0x28, 0x0c, 0x01, 0x06]);
-const LCD_LINE1 = 0x80;
-const LCD_LINE2 = 0xc0;
-const LCD_ENABLE = 0x04;
-const LCD_BACKLIGHT = 0x08;
+const LINE1 = 0x80;
+const LINE2 = 0xc0;
+const ENABLE = 0x04;
+const BACKLIGHT = 0x08;
 
 /*
  * Data is written 4 bits at a time with the lower 4 bits containing the mode.
  */
-function lcdWrite4(data) {
-    rpio.i2cWrite(Buffer.from([(data | LCD_BACKLIGHT)]));
-    rpio.i2cWrite(Buffer.from([(data | LCD_ENABLE | LCD_BACKLIGHT)]));
-    rpio.i2cWrite(Buffer.from([((data & ~LCD_ENABLE) | LCD_BACKLIGHT)]));
+function write4(data) {
+    rpio.i2cWrite(Buffer.from([(data | BACKLIGHT)]));
+    rpio.i2cWrite(Buffer.from([(data | ENABLE | BACKLIGHT)]));
+    rpio.i2cWrite(Buffer.from([((data & ~ENABLE) | BACKLIGHT)]));
 }
-function lcdWrite(data, mode) {
-    lcdWrite4(mode | (data & 0xF0));
-    lcdWrite4(mode | ((data << 4) & 0xF0));
+function write(data, mode) {
+    write4(mode | (data & 0xF0));
+    write4(mode | ((data << 4) & 0xF0));
 }
 
 /*
  * Write a string to the specified LCD line.
  */
-function lcdWriteLine(str, addr) {
-    lcdWrite(addr, 0);
-
-    str.split('').forEach(function (c) {
-        lcdWrite(c.charCodeAt(0), 1);
-    });
-}
-
-function lcdInit(address = 0x27) {
-    rpio.i2cSetSlaveAddress(address);
-    rpio.i2cSetBaudRate(10000);
-    for (var i = 0; i < init.length; i++) {
-        lcdWrite(init[i], 0);
+function writeLine(str, addr, format = false) {
+    write(addr, 0);
+    if (format) {
+        str = formatText(str);
+    }
+    for (let i = 0; i < str.length; i++) {
+        write(str[i], 1);
     }
 }
 
-module.exports = { LCD_LINE1, LCD_LINE2, lcdInit, lcdWriteLine };
+function init(address = 0x27) {
+    rpio.i2cSetSlaveAddress(address);
+    rpio.i2cSetBaudRate(10000);
+    for (var i = 0; i < init.length; i++) {
+        write(init[i], 0);
+    }
+}
+
+function formatText(text) {
+    if (text.length >= 16) {
+        return text.substring(0, 16);
+    } else {
+        return text + " ".repeat(16 - text.length);
+    }
+}
+
+module.exports = { LINE1, LINE2, init, writeLine };
