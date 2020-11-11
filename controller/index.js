f95b61ca1f896142267ab1419a64e8a5fbc67a1a
Haowei Wen <yushijinhun@gmail.com>
2020-11-11 14:07:47 +0800

    controller/lcd: revert 'print to lcd when tag detected'

--- controller/index.js
+++ controller/index.js
@@ -4,27 +4,15 @@ const { TagDetector } = require("./tag_detector");
 const express = require("express");
 const bodyParser = require("body-parser");
 const rpio = require("rpio");
-const lcd = require("./lcd");
 
 const detector = new TagDetector();
-rpio.i2cBegin();
-lcd.init();
-
 detector.on("detect", result => {
     console.log(JSON.stringify(result));
-
-    let text = "";
-    for (const det of result.detections) {
-        text += det.id;
-        text += " ";
-    }
-    lcd.writeLine(text, lcd.LINE1, true);
 });
 
 const app = express();
 app.post("/detector/close", (req, res) => {
     detector.close();
-    rpio.i2cEnd();
     res.sendStatus(204);
 });
 app.post("/gpio/digital", bodyParser.json(), (req, res) => {
