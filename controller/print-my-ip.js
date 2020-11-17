ce69219709692ef5e5cf9931a535ef6087e0dfd1
raspi <raspi-njtech@yushi.moe>
2020-11-17 15:04:09 +0000

    controller: fix print-my-ip

--- controller/print-my-ip.js
+++ controller/print-my-ip.js
@@ -2,28 +2,24 @@
 
 const { spawn } = require("child_process");
 const byline = require("byline");
-const rpio = require("rpio");
 const LCD = require("./lcd");
 
 byline(
     spawn("hostname", ["-I"]).stdout
-).on("data", data => {
+).on("data", async data => {
     const filteredIps = [];
     for (const ip of data.toString().split(" ")) {
         if (/^(\d{1,3}\.){3}\d{1,3}$/.test(ip)) {
             filteredIps.push(ip);
         }
     }
-    rpio.i2cBegin();
 
     const lcd = new LCD();
-    lcd.init();
+    await lcd.init();
     if (filteredIps.length >= 1) {
-        lcd.writeLine(0, filteredIps[0]);
+        await lcd.writeLine(0, filteredIps[0]);
     }
     if (filteredIps.length >= 2) {
-        lcd.writeLine(1, filteredIps[1]);
+        await lcd.writeLine(1, filteredIps[1]);
     }
-
-    rpio.i2cEnd();
 });
