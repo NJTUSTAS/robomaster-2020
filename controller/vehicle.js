49ee2366b00e9861c8af873f11658bf52d23798e
Haowei Wen <yushijinhun@gmail.com>
2020-11-21 15:41:57 +0800

    controller/vehicle: read sensor data

--- controller/vehicle.js
+++ controller/vehicle.js
@@ -1,9 +1,63 @@
 const I2CDevice = require("./i2c");
+const { EventEmitter } = require("events");
+
+const END_OF_MSG = 79;
 
 class Vehicle extends I2CDevice {
     constructor(address = 0x04) {
         super(address);
         this.speedLimit = 1.0;
+
+        this.sensors = new EventEmitter();
+        (async () => {
+            while (true)
+                this._readMessage();
+        })();
+    }
+
+    async _readMessage() {
+        const { bytesRead, buffer } = await this.bus.i2cRead(this.address, 4, Buffer.alloc(4));
+        if (bytesRead === 4) {
+            const command = String.fromCharCode(buffer.readUInt8(0));
+            const data = buffer.readUInt16BE(1);
+            if (buffer.readUInt8(3) === END_OF_MSG) {
+                switch (command) {
+                    case "f":
+                        this._fireDistanceEvent("front", data);
+                        break;
+
+                    case "l":
+                        this._fireDistanceEvent("left", data);
+                        break;
+
+                    case "r":
+                        this._fireDistanceEvent("right", data);
+                        break;
+
+                    default:
+                        console.warn(`vehicle@${address}: unknown message type; ${buffer.toString("hex")}`);
+                        break;
+                }
+            } else {
+                console.warn(`vehicle@${address}: message with no end; ${buffer.toString("hex")}`);
+            }
+        } else {
+            console.warn(`vehicle@${address}: read ${bytesRead}, expected 4; ${buffer.toString("hex")}`);
+        }
+    }
+
+    /**
+     * @param {"front"|"left"|"right"} direction Sensor name
+     * @param {number} distance Distance in mm, -1 if it's too far, -2 if sensor is not connected
+     */
+    _fireDistanceEvent(direction, distance) {
+        if (distance < 0 && distance !== -1 && distance !== -2) {
+            console.warn(`vehicle@${address}: illegal distance ${direction}: ${distance}`);
+        }
+        this.sensors.emit("distance", {
+            direction: direction,
+            distance: distance
+        });
     }
 
     /**
@@ -16,7 +70,7 @@ class Vehicle extends I2CDevice {
             command.charCodeAt(0),
             (data >> 8) & 0xff,
             data & 0xff,
-            79
+            END_OF_MSG
         ]));
         console.debug(`vehicle@${address}: ${command.charAt(0)} ${data}`);
     }
