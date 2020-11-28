38ed84cfca9a8f37028a4bd8aa6f7923e4bb398f
Odysseyer <58683644+Odysseyer@users.noreply.github.com>
2020-11-28 22:44:12 +0800

    Update native.js

--- controller/native.js
+++ controller/native.js
@@ -1,54 +1,122 @@
 const Motor = require("./motor");
 const sleep = require("await-sleep");
 
-
-function go_S(speed) //前进,默认为1档
+const motor = new Motor();
+async function stop()
 {
-    Motor.setSpeed(left_front,speed);
-    Motor.setSpeed(right_front,speed);
-    Motor.setSpeed(left_back,speed);
-    Motor.setSpeed(left_back,speed);
 
-}
-/*function back(speed) //后退，默认一档
-{
-    Motor.setSpeed(left_front,speed);
-    Motor.setSpeed(right_front,speed);
-    Motor.setSpeed(left_back,speed);
-    Motor.setSpeed(left_back,speed);
+    await motor.setSpeed("left_front",0);
+    await motor.setSpeed("right_front",0);
+    await motor.setSpeed("left_back",0);
+    await motor.setSpeed("right_back",0);
 
 }
-function Left(speed)
+
+async function go_ahead(speed,time) 
 {
-    Motor.setSpeed(left_front,speed);
-    Motor.setSpeed(right_front,speed);
-    Motor.setSpeed(left_back,speed);
-    Motor.setSpeed(left_back,speed);
+    await motor.setSpeed("left_front",speed);
+    await motor.setSpeed("right_front",speed);
+    await motor.setSpeed("left_back",speed);
+    await motor.setSpeed("right_back",speed);
+    await sleep(time);
+    await motor.setSpeed("left_front",-speed);
+    await motor.setSpeed("right_front",-speed);
+    await motor.setSpeed("left_back",-speed);
+    await motor.setSpeed("right_back",-speed);
+    await sleep(50);
+    await stop();
 
 }
-function Right(speed)
+async function back(speed,time) 
 {
-    Motor.setSpeed(left_front,speed);
-    Motor.setSpeed(right_front,speed);
-    Motor.setSpeed(left_back,speed);
-    Motor.setSpeed(left_back,speed);
+    await motor.setSpeed("left_front",-speed);
+    await motor.setSpeed("right_front",-speed);
+    await motor.setSpeed("left_back",-speed);
+    await motor.setSpeed("right_back",-speed);
+    await sleep(time);
+    await motor.setSpeed("left_front",speed);
+    await motor.setSpeed("right_front",speed);
+    await motor.setSpeed("left_back",speed);
+    await motor.setSpeed("right_back",speed);
+    await sleep(50);
+    await stop();
+
 
 }
-function Leftgo()
+async function Leftgo(speed,time)
 {
-    Motor.setSpeed(left_front,speed);
-    Motor.setSpeed(right_front,speed);
-    Motor.setSpeed(left_back,speed);
-    Motor.setSpeed(left_back,speed);
-}
-function Rightgo()
-{
-    Motor.setSpeed(left_front,speed);
-    Motor.setSpeed(right_front,speed);
-    Motor.setSpeed(left_back,speed);
-    Motor.setSpeed(left_back,speed);
-}*/
+    await motor.setSpeed("left_front",0.25+speed);
+    await motor.setSpeed("right_front",0.25-speed);
+    await motor.setSpeed("left_back",0.25-speed);
+    await motor.setSpeed("right_back",0.25+speed);
+    await sleep(time);
+    await stop();
 
-go_S(1000);
-await sleep(100);
-go_S(0);
+
+}
+async function Rightgo(speed,time)
+{
+    await motor.setSpeed("left_front",0.25-speed);
+    await motor.setSpeed("right_front",0.25+speed);
+    await motor.setSpeed("left_back",0.25+speed);
+    await motor.setSpeed("right_back",0.25-speed);
+    await sleep(time);
+    await stop();
+}
+
+
+async function Left(speed,time)
+{
+    await motor.setSpeed("left_front",-speed);
+    await motor.setSpeed("right_front",speed);
+    await motor.setSpeed("left_back",speed);
+    await motor.setSpeed("right_back",-speed);
+    await sleep(time);
+    await stop();
+}
+
+async function Right(speed,time)
+{
+    await motor.setSpeed("left_front",speed);
+    await motor.setSpeed("right_front",-speed);
+    await motor.setSpeed("left_back",-speed);
+    await motor.setSpeed("right_back",speed);
+    await sleep(time);
+    await stop();
+
+}
+async function Rightturn(speed,time)
+{
+    await motor.setSpeed("left_front",speed);
+    await motor.setSpeed("right_front",-speed);
+    await motor.setSpeed("left_back",speed);
+    await motor.setSpeed("right_back",-speed);
+    await sleep(time);
+    await stop();
+}
+async function Leftturn(speed,time)
+{
+    await motor.setSpeed("left_front",-speed);
+    await motor.setSpeed("right_front",speed);
+    await motor.setSpeed("left_back",-speed);
+    await motor.setSpeed("right_back",speed);
+    await sleep(time);
+    await stop();
+}
+
+(async () => {
+
+    await motor.begin();
+    await go_ahead(0.5,1000);
+    await Rightturn(0.5,1000);
+
+ 
+
+
+
+})();
+
+module.exports = {
+    begin:motor.begin,
+    go_ahead,back,Left,Leftgo,Leftturn,Right,Rightgo,Rightturn,stop,sleep
+};
